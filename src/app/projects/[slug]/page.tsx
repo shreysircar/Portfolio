e5bbbd6b9575f8dadccea50b082e95b3cd0ea178
shreysircar <sircarshrey@gmail.com>
2026-03-03 12:49:21 +0530

    fixing layout

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,6 +2,12 @@ import { notFound } from "next/navigation";
 import { projects } from "@/data/projects";
 import ProjectTemplate from "@/components/project/ProjectTemplate";
 
+export function generateStaticParams() {
+  return projects.map((project) => ({
+    slug: project.slug,
+  }));
+}
+
 export default function ProjectPage({
   params,
 }: {
